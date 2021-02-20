69ea3148593c318c7499ff10fb7563e31fcda469
Rafael Morelli <kmorelli@gmail.com>
2021-02-20 16:50:20 -0300

    Commit inicial na master

---
+++ src/main/java/br/auriga/App.java
@@ -0,0 +1,13 @@
+package br.auriga;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
